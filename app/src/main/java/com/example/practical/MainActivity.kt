0aa6ab815d77f19fba3998114649e975af70df75
Mayur Gajra <mayurgajra5@gmail.com>
2024-01-27 16:16:49 +0530

    ImageView setup

---
+++ app/src/main/java/com/example/practical/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.practical
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.practical.databinding.ActivityMainBinding
+import java.util.Collections
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addImage = findViewById<Button>(R.id.btnAddImage)
+        val ivImage = findViewById<ImageView>(R.id.ivImage)
+        addImage.setOnClickListener {
+            ivImage.setImageResource(R.drawable.frog1)
+        }
+
+
+    }
+}
