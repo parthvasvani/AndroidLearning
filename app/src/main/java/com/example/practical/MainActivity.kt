735ea8d0360e15ab82cd25292b73bbca648c948a
Mayur Gajra <mayurgajra5@gmail.com>
2024-01-31 19:15:31 +0530

    notification

--- app/src/main/java/com/example/practical/MainActivity.kt
+++ app/src/main/java/com/example/practical/MainActivity.kt
@@ -27,12 +27,20 @@ import java.util.Collections
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -46,43 +54,56 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
+    val CHANNEL_ID = "ChannelID"
+    val CHANNEL_NAME = "ChannelName"
+    val NOTIFICATION_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedPref = getSharedPreferences("myPref",Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-        val btnSave = findViewById<Button>(R.id.btnSave)
-        val etName = findViewById<EditText>(R.id.etName)
-        val etAge = findViewById<EditText>(R.id.etAge)
-        val cbAdult = findViewById<CheckBox>(R.id.cbAdult)
+        createNotificationChannel()
 
-        btnSave.setOnClickListener {
-            val name = etName.text.toString()
-            val age = etAge.text.toString().toInt()
-            val isAdult = cbAdult.isChecked
-
-            editor.apply{
-                putString("name", name)
-                putInt("age", age)
-                putBoolean("isAdult", isAdult)
-                apply()
-            }
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
         }
 
-        val btnLoad = findViewById<Button>(R.id.btnLoad)
-        btnLoad.setOnClickListener {
-            val name = sharedPref.getString("name",null)
-            val age = sharedPref.getInt("age", 0)
-            val isAdult = sharedPref.getBoolean("isAdult",false)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Awesome Notification")
+            .setContentText("This is Context Text")
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
 
-            etName.setText(name)
-            etAge.setText(age.toString())
-            cbAdult.isChecked = isAdult
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        val btnNotification = findViewById<Button>(R.id.btnNotification)
+        btnNotification.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
 
         }
     }
+        fun createNotificationChannel(){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT).apply {
+                        lightColor = Color.GREEN
+                        enableLights(true)
+                }
+
+                val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.createNotificationChannel(channel)
+
+            }
+        }
+
+
 }
+
+
+
 
 
